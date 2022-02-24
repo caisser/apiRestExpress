527078783d2b40a7ec9a7af8de40cc60e5009907
Luis Cardona <luis.cardona@globant.com>
2022-02-24 10:03:01 -0500

    Changes in environment variables

--- db/config.js
+++ db/config.js
@@ -1,16 +1,19 @@
 const { config } = require('../config/config.js');
 
-const USER = encodeURIComponent(config.dbUser);
-const PASSWORD = encodeURIComponent(config.dbPassword);
-const URI = `${config.dbEngine}://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
+// const USER = encodeURIComponent(config.dbUser);
+// const PASSWORD = encodeURIComponent(config.dbPassword);
+// const URI = `${config.dbEngine}://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
 
 module.exports = {
   dev: {
-    url: URI,
+    url: config.dbUrl,
     dialect: config.dbEngine,
   },
   production: {
-    url: URI,
+    url: config.dbUrl,
     dialect: config.dbEngine,
+    ssl: {
+      rejectUnauthorized: false,
+    },
   },
-}
+};
--- libs/sequelize.js
+++ libs/sequelize.js
@@ -2,14 +2,28 @@ const { Sequelize } = require('sequelize');
 const { config } = require('../config/config');
 const setupModels = require('../db/models');
 
-const USER = encodeURIComponent(config.dbUser);
-const PASSWORD = encodeURIComponent(config.dbPassword);
-const URI = `${config.dbEngine}://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
+// let URI = '';
 
-const sequelize = new Sequelize(URI, {
+// if (config.isProd) {
+//   URI = config.dbUrl;
+// } else {
+//   const USER = encodeURIComponent(config.dbUser);
+//   const PASSWORD = encodeURIComponent(config.dbPassword);
+//   URI = `${config.dbEngine}://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
+// }
+
+const options = {
   dialect: config.dbEngine,
-  logging: true,
-});
+  logging: config.isProd ? false : true,
+};
+
+if (config.isProd) {
+  options.ssl = {
+    rejectUnauthorized: false,
+  };
+}
+
+const sequelize = new Sequelize(config.dbUrl, options);
 
 setupModels(sequelize);
 
